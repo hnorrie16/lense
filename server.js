3126656ba0e5457dede04ad9d1bbbb3424d58f21
Hayden <hnorrie18@gmail.com>
2022-02-15 21:15:32 +0200

    please work

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ AWS.config.update({
 app.use(express.static(path.join(__dirname, 'webapp', 'build')));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", “*”);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
   "Access-Control-Allow-Headers",
